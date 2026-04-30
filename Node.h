6dc235100601e141e4e3e3a9925a8f1ada9692c3
Maryam Moustafa <maryam23shabaan@gmail.com>
2026-04-30 17:48:01 +0300

    Update Node.h

--- Node.h
+++ Node.h
@@ -6,13 +6,13 @@ struct Node {
     int data;     
     Node* next;  
     
-    // Default constructor (This fixes the LinkedList and Stack errors!)
+    // Default constructor 
     Node() {
         data = 0;
         next = nullptr;
     }
 
-    // Parameterized constructor (This fixes the Queue error!)
+    // Parameterized constructor
     Node(int val) {
         data = val;
         next = nullptr;
